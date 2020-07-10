8a356d25595b1401dbe4eafebc009b3fa0c47159
jhell85 <jhell85@gmail.com>
2020-07-10 16:29:13 -0700

    add failing 2nd test

--- src/reducers/__tests__/drink-list-reducer.test.js
+++ src/reducers/__tests__/drink-list-reducer.test.js
@@ -1,7 +1,13 @@
 import drinkListReducer from '../drink-list-reducer'
+import { v4 } from "uuid";
 
 describe('drinkListReducer', () =>{
   test('should return default state if there is no action passed into reducer', () => {
     expect(drinkListReducer([], {type: null})).toEqual([]);
-  })
+  });
+
+  test ('should return an array with one drink added to it', ()=> {
+    const drink = {name: "Hop Master", brand: "beer land", alcoholContent: 5, price: 2, drinksLeft: 124, timeStamp: new Date(), id: v4()}
+    expect(drinkListReducer(drink, {type: 'ADD_DRINK'})).toEqual([{name: "Hop Master", brand: "beer land", alcoholContent: 5, price: 2, drinksLeft: 124, timeStamp: new Date(), id: v4()}])
+  });
 })
